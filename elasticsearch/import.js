66c5b286df69cd8519c9302c051128d4fb709aa4
Ruth BOUA <ruthboua@gmail.com>
2021-03-24 10:04:34 +0100

    import.js

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -2,6 +2,7 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const { Client } = require('@elastic/elasticsearch');
+const { type } = require('os');
 
 const ELASTIC_SEARCH_URI = 'http://localhost:9200';
 const INDEX_NAME = '911-calls';
@@ -18,24 +19,45 @@ async function run() {
   await client.indices.create({
     index: INDEX_NAME,
     body : {
+      "lat" :  {"type":"geo_point"},
+      "lng":  {"type":"geo_point"},
+      "timeStamp" :  {"type":"date"}
       // TODO configurer l'index https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html
     }
   });
-
+let calls = [];
   fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
       const call = { 
+        "lat":data.lat,
+        "lng":data.lng,
+        "title":data.title,
+        "zip":data.zip,
+        "timeStamp":data.timeStamp,
+        "twp":data.twp,
+        "addr":data.addr
       };
+      calls.push(call);
       // TODO créer l'objet call à partir de la ligne
     })
     .on('end', async () => {
+      client.bulk(createBulkInsertQuery(calls), (err, resp) => {
+        if (err) console.trace(err.message);
+        else console.log(`Inserted ${resp.body.items.length} calls`);
       // TODO insérer les données dans ES en utilisant l'API de bulk https://www.elastic.co/guide/en/elasticsearch/reference/7.x/docs-bulk.html
+      });
     });
-  
+    
+    function createBulkInsertQuery(calls) {
+      const body = calls.reduce((acc, call) => {
+        acc.push({ index: { _index: INDEX_NAME, _type: '_doc' } })
+        acc.push(call)
+        return acc
+      }, []);
 
+      return { body };
+    }
+    
 }
-
 run().catch(console.log);
-
-
